6b6ef8a20a508ce793ff02058a6290824e2f43b0
Drake Witt <dwitt@dranweb.com>
2014-08-28 09:26:51 -0400

    Ideekay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,1 @@
-$(function() {
-    var BV = new $.BigVideo();
-    BV.init();
-    BV.show('http://vjs.zencdn.net/v/oceans.mp4',{ambient:true});
-});
+console.log("Oh, Hello. You must be a developer or a wonder soul.");
